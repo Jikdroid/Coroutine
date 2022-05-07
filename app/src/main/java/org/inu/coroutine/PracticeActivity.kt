cde2c9c2ee4e1ce52cbe3878844cadd73fe8d793
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-08 01:55:50 +0900

    #2 - 4. Dispatcher와 launch, async를 이용해 Coroutine 붙이기 1. 결과 반환이 필요 없을 때는 launch를 필요할 때는 async를 사용한다. 2. 코루틴을 생성할 때 Dispatcher을 설정하는 것만으로 Dispatcher의 전환이 가능하다.

--- app/src/main/java/org/inu/coroutine/PracticeActivity.kt
+++ app/src/main/java/org/inu/coroutine/PracticeActivity.kt
@@ -4,29 +4,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import kotlinx.coroutines.*
+import org.inu.coroutine.databinding.ActivityPracticeBinding
 
-class PracticeActivity : AppCompatActivity() ,Runnable{
-    val button : Button by lazy {
-        findViewById(R.id.btn)
-    }
+class PracticeActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityPracticeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_practice)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_practice)
+        binding.activity = this
 
-
-       button.setOnClickListener {
-           Thread(this).start()
-           Thread(this).start()
-       }
     }
 
-    override fun run() {
-        Log.d(TAG, "Runnable Running")
-        Log.d(TAG, Thread.currentThread().toString())
+    fun launch(){
+        with(CoroutineScope(Dispatchers.IO)) {
+            val job:Job = launch { println(1) }
+        }
     }
 
-    companion object{
-        const val TAG = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    fun async(){
+        CoroutineScope(Dispatchers.Main).launch {
+            val deferredInt: Deferred<Int> = async {
+                println(2)
+                1
+            }
+            val value = deferredInt.await()
+            println(value)
+        }
     }
+
+    fun switchDispatcher(){
+        CoroutineScope(Dispatchers.Main).launch {
+
+            // 1. 데이터 입출력을 해야 하므로 IO Dispatcher에 배분
+            val deferredInt: Deferred<Array<Int>> = async(Dispatchers.IO){
+                println(1)
+                arrayOf(3,1,2,4,5)
+            }
+
+            // 2. Sort해야 하므로 CPU작업을 많이 해야하는 Default Dispatcher에 배분
+            val sortedDeferred = async(Dispatchers.Default) {
+                val value  = deferredInt.await()
+                value.sortedBy { it }
+            }
+
+            // 3. 설정하지 않으면 기본 Dispatcher인 Main Dispatcher에 보내진다.
+            // 3. TextView에 세팅하는 것은 UI 작업이므로 Main Dispatcher에 배분
+            val textviewSettingJob = launch {
+                val sortedArray = sortedDeferred.await()
+                binding.btn.text = sortedArray.toString()
+            }
+        }
+    }
+
 }
 
