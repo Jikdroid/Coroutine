b24e536999edafb57d4185446b7bee9fc3721026
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-08 02:23:22 +0900

    #2 - 5. suspend fun의 이해 1. 코루틴의 일시 중단 기능은 코루틴 내부에서만 수행할 수 있다. 2. suspend fun는 일시 중단 가능한 함수로, 해당 함수 내에 일시 중단이 가능한 작업이 있다는 것을 뜻한다. 3. 따라서 suspend fun은 코루틴 내부에서 또는 suspend fun 내부에서만 사용할 수 있다.

--- app/src/main/java/org/inu/coroutine/PracticeActivity.kt
+++ app/src/main/java/org/inu/coroutine/PracticeActivity.kt
@@ -16,46 +16,87 @@ class PracticeActivity : AppCompatActivity(){
         binding = DataBindingUtil.setContentView(this, R.layout.activity_practice)
         binding.activity = this
 
+        // exampleSuspend2()
+        // suspend fun는 (코루틴이)일시중단 가능한 함수를 지칭하는 것이기 때문에, 해당 함수는 무조건 코루틴 내부에서 수행되어야 하기 때문에 오류 발생
+        CoroutineScope(Dispatchers.Main).launch {
+            exampleSuspend2()
+        }
+
     }
 
-    fun launch(){
-        with(CoroutineScope(Dispatchers.IO)) {
-            val job:Job = launch { println(1) }
+    // 일시중단 함수(suspend fun)는 당연히 일시중단 함수(suspend fun) 내부에서 사용될 수 있다.
+    // 당연히 가장 바깥에 있는 일시중단 함수(아래 코드에서는 exampleSuspend2)는 코루틴 내부에서 실행되어야 한다.
+    suspend fun exampleSuspend4(){
+        exampleSuspend3()
+    }
+
+    suspend fun exampleSuspend3() {
+        val job3 = CoroutineScope(Dispatchers.IO).async {
+            (1..10000).sortedByDescending { it }
+        }
+
+        job3.await()
+    }
+
+
+
+    suspend fun exampleSuspend2(){
+
+        val job3 = CoroutineScope(Dispatchers.Default).async {
+            // 2. IO Thread에서 작업3를 수행한다.
+            (1..1000).sortedByDescending { it }
+        }
+
+        job3.await()
+
+        val job1 = CoroutineScope(Dispatchers.Main).launch {
+            // 1. Main Thread에서 작업1을 수행한다.
+            println(1)
+
+            // 3. 작업1의 남은 작업을 위해 작업3로부터 결과값이 필요하기 때문에 Main Thread는 작업1을 일시중단한다.
+            val job3Result = job3.await()
+            // 6. 작업3로부터 결과를 전달받는다.
+
+            // 7. 작업1이 재개된다.
+            job3Result.forEach {
+                println(it)
+            }
+        }
+
+        // 4. Main Thread에서 작업2이 수행되고 완료된다.
+        val job2 = CoroutineScope(Dispatchers.Main).launch {
+            println("job2 수행완료")
         }
     }
 
-    fun async(){
-        CoroutineScope(Dispatchers.Main).launch {
-            val deferredInt: Deferred<Int> = async {
-                println(2)
-                1
-            }
-            val value = deferredInt.await()
-            println(value)
+
+    fun exampleSuspend1(){
+
+        val job3 = CoroutineScope(Dispatchers.Default).async {
+            // 2. IO Thread에서 작업3를 수행한다.
+            (1..1000).sortedByDescending { it }
         }
-    }
 
-    fun switchDispatcher(){
-        CoroutineScope(Dispatchers.Main).launch {
+        //job3.await()    // await 정지하고 job3의 결과값을 받아오는데 그럴려면 코루틴 내부에 있어야하거나 일시정지가 가능한 함수(suspend fun)
+                          // 안에 있어햐한다.(코루틴이 일시중단이 되려면 수행되는 위치 또한 코루틴 내부여야 하기 때문)
 
-            // 1. 데이터 입출력을 해야 하므로 IO Dispatcher에 배분
-            val deferredInt: Deferred<Array<Int>> = async(Dispatchers.IO){
-                println(1)
-                arrayOf(3,1,2,4,5)
+        val job1 = CoroutineScope(Dispatchers.Main).launch {
+            // 1. Main Thread에서 작업1을 수행한다.
+            println(1)
+
+            // 3. 작업1의 남은 작업을 위해 작업3로부터 결과값이 필요하기 때문에 Main Thread는 작업1을 일시중단한다.
+            val job3Result = job3.await()
+            // 6. 작업3로부터 결과를 전달받는다.
+
+            // 7. 작업1이 재개된다.
+            job3Result.forEach {
+                println(it)
             }
+        }
 
-            // 2. Sort해야 하므로 CPU작업을 많이 해야하는 Default Dispatcher에 배분
-            val sortedDeferred = async(Dispatchers.Default) {
-                val value  = deferredInt.await()
-                value.sortedBy { it }
-            }
-
-            // 3. 설정하지 않으면 기본 Dispatcher인 Main Dispatcher에 보내진다.
-            // 3. TextView에 세팅하는 것은 UI 작업이므로 Main Dispatcher에 배분
-            val textviewSettingJob = launch {
-                val sortedArray = sortedDeferred.await()
-                binding.btn.text = sortedArray.toString()
-            }
+        // 4. Main Thread에서 작업2이 수행되고 완료된다.
+        val job2 = CoroutineScope(Dispatchers.Main).launch {
+            println("job2 수행완료")
         }
     }
 
