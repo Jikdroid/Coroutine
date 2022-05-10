559bb2002313f7dbd845ca406ec4203221154fa7
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-10 16:17:49 +0900

    #2 - *3. withContext 사용해 join, await 대체하기 withContext를 이용하면 결과값 수신이 필요한 코드의 순차화가 가능하다.

--- app/src/main/java/org/inu/coroutine/PracticeActivity.kt
+++ app/src/main/java/org/inu/coroutine/PracticeActivity.kt
@@ -20,26 +20,31 @@ class PracticeActivity : AppCompatActivity(){
 
     fun executeSuspendFun(){
         CoroutineScope(Dispatchers.Main).launch {
-            deferredException()
+            jobException()
         }
     }
 
-    private suspend fun deferredException(){
-        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            when (throwable){
-                is IllegalArgumentException -> println("More Argument Needed To Process Job")
-                is InterruptedException -> println("Job Interrupted")
-            }
-        }
+    private suspend fun jobException(){
 
-
-        val deferred = CoroutineScope(Dispatchers.IO).async(exceptionHandler) {
-            throw IllegalArgumentException()
-            arrayOf(1,2,3)
+        val deferred:Deferred<String> = CoroutineScope(Dispatchers.IO).async {
+            "Deferred Result"
         }
-        CoroutineScope(Dispatchers.IO).launch(exceptionHandler) {
-            deferred.await()
-        }.join()
+        println(deferred.await())
+
+        val result : String = withContext(Dispatchers.IO){
+            "Deferred Result"
+        }
+        println(result)
+        // 1. withContext 블록은 IO Thread의 작업이 끝나기 전까지 Main Thread에서 수행되는 코루틴을 일시중단되도록 만든다.
+
+        // 2. IO Thread의 작업이 끝나면 "Async Result"가 반환되며 이는 result에 세팅된다.
+
+        // 3. result가 프린트된다.
+
+        // 한계점
+        // withContext를 너무 많이 사용하는 것은 좋지 않아 보인다.
+        // 비동기 작업을 하는 이유 자체가 순차적인 처리를 했을 때 연산 시간을 줄이기 위함인데,
+        // 순차적으로 처리하도록 만들어버리면 비동기 작업의 장점이 사라지기 때문이다.
     }
 }
 
